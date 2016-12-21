04790025daf967cbcf3ccd908b55f52add956719
Shivateja Madipalli <bighairdev@Shivatejas-MacBook-Pro-2.local>
2016-12-21 03:08:25 -0800

    Implementing node promise

--- api/resolver.js
+++ api/resolver.js
@@ -1,47 +1,62 @@
-var access_token = require('../utils/key');
-var bitly_endpoint = require('../utils/bitlyendpoint');
-var express = require('express');
-var Client = require("node-rest-client").Client;
-var bodyParser = require('body-parser');
+let express = require('express');
+let Client = require("node-rest-client").Client;
+let bodyParser = require('body-parser');
+let config = require('../config.json');
 
-var client = new Client();
-var app = express();
+let Promise = require('promise');
+
+const BITLY_ENDPOINT = config.bitly_endpoint;
+const ACCESS_TOKEN = config.key;
+
+let client = new Client();
+let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+let shortenURL = function(bitlyRequest){
+  return new Promise(function(resolve, reject) {
+    let bitlyRes = client.get(bitlyRequest, function(data, response) {
+      console.log("sent",bitlyRequest);
+
+      if(Buffer.isBuffer(data)) {
+        data = data.toString('utf8');
+      }
+      console.log("data,res ",data,response);
+      if(response.statusCode == '200') {
+        console.log("received ,",data);
+        resolve(data);
+      }
+    }).on('error', function (err) {
+        reject(err);
+    });
+  });
+};
+
 // The API that returns the in-email representation.
 module.exports = function(req, res) {
+  console.log("here");
   let original_url = req.query.text.trim();
-  try {
-    let get_url_shortened = bitly_endpoint;
+    let get_url_shortened = BITLY_ENDPOINT;
     let original_url_encoded = formatOriginalURL(original_url);
 
     get_url_shortened += '/v3/shorten?access_token=' +
-                          access_token +
+                          ACCESS_TOKEN +
                           '&longUrl=' +
                           original_url_encoded +
                           '&format=txt';
 
-    let bitlyRes = client.get(get_url_shortened, function(data, response) {
-      if(Buffer.isBuffer(data)) {
-        data = data.toString('utf8');
-      }
-      if(response.statusCode == '200') {
-        let anchor_tag = '<a href="' + data + '">' + data + '</a>';
-        res.status(response.statusCode).json({
-          body: anchor_tag,
-          raw: true
-        });
-      }
-    }).on('error', function (err) {
-      res.status(500).send('Error');
+    //resolved here
+    shortenURL(get_url_shortened).then(function(data){
+      console.log("data in resolve",data);
+      let anchor_tag = '<a href="' + data + '">' + data + '</a>';
+      res.status(200).json({
+        body: anchor_tag,
+        raw: true
+      });
+
     });
-  }
-  catch(e) {
-    console.log('Exception: ', e);
-    res.status(500).send('Error');
-  }
 };
 
 
